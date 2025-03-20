159793e7b57afe2e777e8155a616a1476b71ffc6
JihyunKim <kimwlgus001130@gmail.com>
2025-03-20 10:44:50 +0900

    edit for fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import AcmeLogo from "@/app/ui/acme-logo";
-import { lusitana } from "./ui/font";
+import { lusitana } from "./ui/fonts";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import styles from "@/app/ui/home.module.css";
